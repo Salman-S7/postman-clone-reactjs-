f6dc12d69beecc51ca632f8610118e30a50fb4a4
Salman-S7 <sammyzed1505@gmail.com>
2024-06-28 19:07:58 +0530

    header complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
+    <div className='font-sans-'>
+    <Header/>
+    </div>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { FaCircleUser } from "react-icons/fa6"
+
+const Header = () => {
+  return (
+      <header className="h-[10vh] w-full bg-slate-600 text-white fixed top-0 left-0 flex
+     items-center justify-between px-12">
+          <div className="text-orange-500 font-black ">Postman</div>
+          <div className=""><FaCircleUser className="h-8 w-8"/></div>
+    </header>
+  )
+}
+
+export default Header
